7ddc1135af20490219faa6f8a7614654397ea1a3
Kitty-Song <alice@sweeneyclan.org>
2026-09-04 14:08:21 -0700

    new work

--- myscript.js
+++ myscript.js
@@ -6,4 +6,11 @@ $(document).ready(function() {
 		$("p").addClass("bright");
 		$("img").addClass("border");
 	});
+	$("p").click(function() {
+		$("p:not(#p1)").addClass("gone");
+		
+	})
+	$("p").mouseleave(function() {
+		$("h1").addClass("font");
+	})
 });
